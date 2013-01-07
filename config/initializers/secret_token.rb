950e36d3cb4494b9c8e69d669570fd4d07568ec4
Jack Chatterton <jack@substantial.com>
2013-01-07 14:02:40 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yumster::Application.config.secret_token = 'cbadf8b9f911c488ed25b1a9e1a6ae88bab770983130d3e7d72b079217084d0b970aaa7dc5e79cc52b086c0a87e9a7d425f8615ed73244d7120ffea835f2e1d9'
