d8e67363a91e5e22f7bbfd8af99144fc23a792c6
Jack C <chattj@gmail.com>
2013-01-12 16:25:33 -0800

    add fields to location model

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,3 +1,14 @@
 class Location < ActiveRecord::Base
   attr_accessible :description, :latitude, :longitude
+
+  validates :description, presence: true
+  validates :latitude, presence: true, :numericality => {
+    :greater_than_or_equal_to => -90,
+    :less_than_or_equal_to => 90
+  }
+  validates :longitude, presence: true, :numericality => {
+    :greater_than_or_equal_to => -180,
+    :less_than_or_equal_to => 180
+  }
+
 end
