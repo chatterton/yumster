ee866c20cab90933eafc30bfc4adc677f038bd28
Jack C <chattj@gmail.com>
2013-01-15 22:17:35 -0800

    error messages for new location form

--- spec/views/locations/new_spec.rb
+++ spec/views/locations/new_spec.rb
@@ -1,10 +1,12 @@
 require 'spec_helper'
 
 describe "locations/new" do
-  it 'displays a form with the right fields' do
-    location = mock_model Location
-    assign(:location, location)
+  before do
+    @location = mock_model Location
+    assign(:location, @location)
+  end
 
+  it 'displays a form with the right fields' do
     render
 
     # TODO: what is broken here?
@@ -14,6 +16,19 @@ describe "locations/new" do
     rendered.should have_selector("form input#location_longitude[type='text']")
     rendered.should have_selector("form input#location_description[type='text']")
     rendered.should have_selector("form input[type='submit']")
-
   end
+
+  describe "when there are errors" do
+    before do
+      errors = double(:full_messages => ["error1","error2"], :any? => true).as_null_object
+      @location.stub!(:errors).and_return errors
+    end
+    it "displays them to the user" do
+      render
+
+      rendered.should have_content "error1"
+      rendered.should have_content "error2"
+    end
+  end
+
 end
