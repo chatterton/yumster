6fdb46be0441bfa5e115435fc9a0b1b1b8ae8b28
Jack C <chattj@gmail.com>
2013-01-29 02:19:10 -0800

    link from home page to nearby & add

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -2,9 +2,16 @@ require 'spec_helper'
 
 describe "static pages" do
   describe "home page" do
-    it 'should say Yumster somewhere on it' do
+    before do
       visit '/pages/home'
-      page.should have_content 'Yumster'
+    end
+    subject { page }
+    it { should have_content 'Yumster' }
+    it 'should link to nearby locations' do
+      page.should have_link "Find nearby locations", :href => "/locations"
+    end
+    it 'should link to create a location' do
+      page.should have_link "Add a location", :href => "/locations/new"
     end
   end
 end
