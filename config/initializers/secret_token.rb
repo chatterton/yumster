0148e736f9220194a6e2123dba510d02c865a900
Jack C <chattj@gmail.com>
2013-02-05 21:47:19 -0800

    read secret_token from env

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,9 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Yumster::Application.config.secret_token = 'cbadf8b9f911c488ed25b1a9e1a6ae88bab770983130d3e7d72b079217084d0b970aaa7dc5e79cc52b086c0a87e9a7d425f8615ed73244d7120ffea835f2e1d9'
+
+if Rails.env.production? && ENV['SECRET_TOKEN'].blank?
+    raise 'SECRET_TOKEN environment variable must be set!'
+end
+
+Yumster::Application.config.secret_token = ENV['SECRET_TOKEN'] || 'cbadf8b9f911c488ed25b1a9e1a6ae88bab770983130d3e7d72b079217084d0b970aaa7dc5e79cc52b086c0a87e9a7d425f8615ed73244d7120ffea835f2e1d9'
