1be50ae96360b4dffde59e8ffb66b40f627f9e31
Jack C <chattj@gmail.com>
2013-02-06 12:34:21 -0800

    migrate to add username to database

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130206043124) do
+ActiveRecord::Schema.define(:version => 20130206202315) do
 
   create_table "locations", :force => true do |t|
     t.string   "description"
@@ -39,10 +39,12 @@ ActiveRecord::Schema.define(:version => 20130206043124) do
     t.string   "unconfirmed_email"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
+    t.string   "username",                               :null => false
   end
 
   add_index "users", ["confirmation_token"], :name => "index_users_on_confirmation_token", :unique => true
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
+  add_index "users", ["username"], :name => "index_users_on_username", :unique => true
 
 end
