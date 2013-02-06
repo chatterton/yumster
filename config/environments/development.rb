c1ee96656a54842910c6c78962195081a5b8e4fc
Jack C <chattj@gmail.com>
2013-02-05 20:06:56 -0800

    set default email options

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,7 @@ Yumster::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  # Mail from dev will point to this address
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,8 @@ Yumster::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  # Mail from production will point to this address
+  config.action_mailer.default_url_options = { :host => 'yumster.co' }
+
 end
