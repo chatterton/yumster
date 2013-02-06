b4368d59ad1a50bba01025e7d260a54e6dc4798e
Jack C <chattj@gmail.com>
2013-02-05 21:31:56 -0800

    configure sendmail for dev

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,9 +13,6 @@ Yumster::Application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
-  # Don't care if the mailer can't send
-  config.action_mailer.raise_delivery_errors = false
-
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
 
@@ -37,4 +34,10 @@ Yumster::Application.configure do
 
   # Mail from dev will point to this address
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
+  # Mailer options
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :sendmail
+
 end
