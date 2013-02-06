7104f1a8c972b8b93bdff3135c2b07c59ef6f6c3
Jack C <chattj@gmail.com>
2013-02-05 23:54:36 -0800

    add mandrill conf to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,3 +69,12 @@ Yumster::Application.configure do
   config.action_mailer.default_url_options = { :host => 'yumster.co' }
 
 end
+ActionMailer::Base.smtp_settings = {
+  :port =>           '587',
+  :address =>        'smtp.mandrillapp.com',
+  :user_name =>      ENV['MANDRILL_USERNAME'],
+  :password =>       ENV['MANDRILL_APIKEY'],
+  :domain =>         'heroku.com',
+  :authentication => :plain
+}
+ActionMailer::Base.delivery_method = :smtp
