5d11481730f0c23733dabb7ffd13ad54eb4da7ae
Jack C <chattj@gmail.com>
2013-02-09 14:19:51 -0800

    add log out link on user's own page

--- spec/views/users/show_spec.rb
+++ spec/views/users/show_spec.rb
@@ -7,6 +7,8 @@ describe "users/show" do
     loc1 = FactoryGirl.create :location, user: @user
     loc2 = FactoryGirl.create :location, user: @user
     assign(:user, @user)
+    view.stub(:user_signed_in?).and_return(true)
+    view.stub(:current_user).and_return(@user)
   end
 
   it "should show the user's username" do
@@ -17,6 +19,26 @@ describe "users/show" do
   it "should show the user's location count" do
     render
     rendered.should =~ /2 locations/
+  end
+
+  context "when a user is signed in" do
+    before do
+      view.stub(:current_user).and_return(@user)
+    end
+    it "should show a log out link" do
+      render
+      rendered.should have_link '', href: destroy_user_session_path
+    end
+  end
+  context "when a user is somebody else" do
+    before do
+      user2 = FactoryGirl.create :user, :username => 'teddy', :email => 'x@ya.com'
+      view.stub(:current_user).and_return(user2)
+    end
+    it "should not show a log out link" do
+      render
+      rendered.should_not have_link '', href: destroy_user_session_path
+    end
   end
 
 end
