450fba10c0dba836acd6d40a505c2edf226b7e30
Jack C <chattj@gmail.com>
2013-02-22 19:24:24 -0800

    attributes for tip model

--- app/models/tip.rb
+++ app/models/tip.rb
@@ -1,6 +1,8 @@
 class Tip < ActiveRecord::Base
-  attr_accessible :text
+  attr_accessible :text, :user_id
+  attr_readonly :location_id
   belongs_to :user
   belongs_to :location
-  #validates_presence_of :text
+  validates_presence_of :text
+  validates_uniqueness_of :user_id, :scope => :location_id
 end
--- spec/models/tip_spec.rb
+++ spec/models/tip_spec.rb
@@ -2,15 +2,32 @@ require 'spec_helper'
 
 describe Tip do
   before do
-    location = nil
-    user = FactoryGirl.create :user do |u|
-      location = u.locations.create FactoryGirl.attributes_for(:location)
-    end
-    @tip = location.tips.create(text: "some text")
+    @user = FactoryGirl.create :user
+    @location = @user.locations.create FactoryGirl.attributes_for(:location)
+    @tip = @location.tips.create(text: "some text", user_id: @user.id)
   end
 
   subject { @tip }
 
   it { should be_valid }
+
+  describe "readonly attributes" do
+    it "should successfully return user_id and location_id" do
+      @tip.location_id.should be
+      @tip.user_id.should be
+    end
+  end
+
+  describe "when text is not present" do
+    before { @tip.text = " " }
+    it { should_not be_valid }
+  end
+
+  describe "when the same user creates a tip on the same location" do
+    before { @tip2 = @location.tips.create(text: "some other text", user_id: @user.id) }
+    it "should not be valid" do
+      @tip2.should_not be_valid
+    end
+  end
 
 end
