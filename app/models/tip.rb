4777b474aa0e54e8704c47635ca946e51370d260
Jack C <chattj@gmail.com>
2013-02-26 01:07:53 -0800

    add max length to tip

--- app/models/tip.rb
+++ app/models/tip.rb
@@ -4,4 +4,5 @@ class Tip < ActiveRecord::Base
   belongs_to :location, :counter_cache => true
   validates_presence_of :text, :location_id, :user_id
   validates_uniqueness_of :user_id, :scope => :location_id
+  validates_length_of :text, :minimum => 5, :maximum => 200
 end
