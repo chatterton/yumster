92afedcbd6a61c0d8ad5b390fe606862fe730d43
Jack C <chattj@gmail.com>
2013-03-22 15:20:45 -0700

    rails g forem:install (ran x2 for bug)

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Yumster::Application.routes.draw do
 
+  # This line mounts Forem's routes at /forums by default.
+  # This means, any requests to the /forums URL of your application will go to Forem::ForumsController#index.
+  # If you would like to change where this extension is mounted, simply change the :at option to something different.
+  #
+  # We ask that you don't use the :as option here, as Forem relies on it being the default of "forem"
+  mount Forem::Engine, :at => '/forums'
+
+
   get "admin/locations"
   put "admin/locations/:id/approve" => "admin#approve", :as => "approve_location"
 
